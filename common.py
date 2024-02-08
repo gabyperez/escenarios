1582ae7209b20274c4590d79bcf65b3d429442a8
gabyperez <51555150+gabyperez@users.noreply.github.com>
2024-02-08 11:13:16 -0300

    Update common.py

--- common.py
+++ common.py
@@ -22,20 +22,23 @@ def imprimir_lista(lista):
       print(f"Escenario {indice+1}: {oracion}")
 
 def get_consultas_respuestas_esperadas():
-  consultas_respuestas = [["Realizar fumigación para controlar plagas", 10, 13, 16, 17, 23, 24, 37, 38, 53, 61],
-                          ["Recortar ramas de la planta", 11, 33, 39, 65], 
-                          ["Echar agua a la planta", 12, 41, 54, 55, 57],
-                          ["Recolectar tomates", 20, 21, 42, 64], 
-                          ["Quitar las malas hierbas", 3, 8, 58], 
-                          ["Eliminar hongo de las plantas", 0], 
-                          ["Eliminar pestes de la planta",0], 
-                          ["Germinar semilla", 0], 
-                          ["Fertilizar la tierra", 0], 
-                          ["limpiar hierbas no deseadas", 0], 
-                          ["colocar soportes a una planta",0], 
-                          ["Generar abono orgánico",0], 
-                          ["Hidratar las semillas de tomate", 0], 
-                          ["Aportar agua a las plantas", 0] , 
-                          ["Pasar planta del almácigo a la tierra", 0], 
-                          ["Curar planta enferma con hongos", 0] ]
+  consultas_respuestas_diccionario = {
+    "Realizar fumigación para controlar plagas": ["Control de plaga", "Control enfermedades bacterianas", "Manejo enfermedades fungosas", "Controlar plagas e insectos", "Control de enfermedades virales"],
+    "Recortar ramas de la planta": ["Despunte de inflorescencias", "Podar planta de tomate", "Podar manualmente"],
+    "Echar agua a la planta": ["Regar", "Regar la huerta "],
+    "Recolectar tomates": ["Cosechar tomates"],
+    "Quitar las malas hierbas": ["Desmalezar", "Deshierbe", "Quita de malezas"],
+    "Eliminar hongo de las plantas": [0],
+    "Eliminar pestes de la planta": [0],
+    "Germinar semilla": [0],
+    "Fertilizar la tierra": [0],
+    "limpiar hierbas no deseadas": [0],
+    "colocar soportes a una planta": [0],
+    "Generar abono orgánico": [0],
+    "Hidratar las semillas de tomate": [0],
+    "Aportar agua a las plantas": [0],
+    "Pasar planta del almácigo a la tierra": [0],
+    "Curar planta enferma con hongos": [0]
+}
+
   return consultas_respuestas
